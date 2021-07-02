75efeba8074ceab2850c6249a5b9b6e016df8ae0
Ravi Patel <ravikano1997@gmail.com>
2021-07-02 22:46:29 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,22 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
+import 'package:json_example/json_online.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "JSON Example",
       home: Scaffold(
         appBar: AppBar(
           title: Text("JSON"),
         ),
-        body: Container(
-          child: FutureBuilder(
-            future: DefaultAssetBundle.of(context)
-                .loadString('load_json/person.json'),
-            builder: (context, snapshot) {
-              var mydata = jsonDecode(snapshot.data.toString());
-
-              return ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text("Name: " + mydata[index]['name']),
-                        Text("Age: " + mydata[index]['age']),
-                        Text("Gender: " + mydata[index]['gender']),
-                      ],
-                    ),
-                  );
-                },
-                itemCount: mydata == null ? 0 : mydata.length,
-              );
-            },
-          ),
-        ),
+        body: OnlineJSON(),
       ),
     );
   }
