7aba63b8c6402f2ee2b0ece9f489d8410f09141f
Ravi Patel <ravikano1997@gmail.com>
2021-07-02 22:47:28 +0530

    Second Commit

---
+++ lib/json_online.dart
@@ -0,0 +1,84 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class OnlineJSON extends StatefulWidget {
+  const OnlineJSON({Key? key}) : super(key: key);
+
+  @override
+  _OnlineJSONState createState() => _OnlineJSONState();
+}
+
+class _OnlineJSONState extends State<OnlineJSON> {
+  final String url = "https://api.covid19india.org/data.json";
+  late List data;
+  bool isData = false;
+
+  @override
+  void initState() {
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+    var response =
+        await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var convertDataToJson = jsonDecode(response.body);
+      data = convertDataToJson['statewise'];
+      isData = true;
+    });
+
+    return "Success";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isData
+        ? ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Card(
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("State: "),
+                                Text("Active Case: "),
+                                Text("Confirmed Case: "),
+                                Text("Total death: "),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(data[index]['state']),
+                                Text(data[index]['active']),
+                                Text(data[index]['confirmed']),
+                                Text(data[index]['deaths']),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            })
+        : Center(
+            child: CircularProgressIndicator(),
+          );
+  }
+}
---
+++ lib/local_json.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class LocalJSON extends StatefulWidget {
+  const LocalJSON({Key? key}) : super(key: key);
+
+  @override
+  _LocalJSONState createState() => _LocalJSONState();
+}
+
+class _LocalJSONState extends State<LocalJSON> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+        future: DefaultAssetBundle.of(context)
+            .loadString('load_json/person.json'),
+        builder: (context, snapshot) {
+          var mydata = jsonDecode(snapshot.data.toString());
+
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text("Name: " + mydata[index]['name']),
+                    Text("Age: " + mydata[index]['age']),
+                    Text("Gender: " + mydata[index]['gender']),
+                  ],
+                ),
+              );
+            },
+            itemCount: mydata == null ? 0 : mydata.length,
+          );
+        },
+      ),
+    );
+  }
+}
